b124c2e19c66dfc7c6e08ca3c792fd134beaae8d
abby3010 <abhayubhale.30@gmail.com>
2020-06-10 17:44:13 +0530

    Hosted site on fooduko gmail address

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fooduko/constants.dart';
 import 'package:fooduko/screens/home_screen.dart';
 import 'package:fooduko/screens/login_screen.dart';
 import 'package:fooduko/screens/signup_screen.dart';
+import 'package:fooduko/screens/take_info_screen.dart';
 import 'package:fooduko/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
         LoginScreen.id: (context) => LoginScreen(),
         SignupScreen.id: (context) => SignupScreen(),
         HomeScreen.id: (context) => HomeScreen(),
+        TakeInfoScreen.id: (context) => TakeInfoScreen(),
       },
     );
   }
